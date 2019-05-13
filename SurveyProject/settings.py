a69131387c4b14e3abefb539ee5e20d6279ca927
priyanshdeshmukh <47139684+priyanshdeshmukh@users.noreply.github.com>
2019-05-13 10:49:05 +0530

    Update settings.py

--- SurveyProject/settings.py
+++ SurveyProject/settings.py
@@ -105,8 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'priyanshdeshmukh06@gmail.com'
-EMAIL_HOST_PASSWORD = 'pihuuyuj06'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 
